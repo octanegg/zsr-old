ebe526b0f519a74f4d38996fe4644f4613c2215d
Slokh <slokh@octane.gg>
2021-04-03 02:36:03 -0400

    Add substitute and coach fields for player

--- octane/types.go
+++ octane/types.go
@@ -147,12 +147,14 @@ type Car struct {
 
 // Player .
 type Player struct {
-	ID       *primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Tag      string              `json:"tag,omitempty" bson:"tag,omitempty"`
-	Name     string              `json:"name,omitempty" bson:"name,omitempty"`
-	Country  string              `json:"country,omitempty" bson:"country,omitempty"`
-	Team     *Team               `json:"team,omitempty" bson:"team,omitempty"`
-	Accounts []*Account          `json:"accounts,omitempty" bson:"accounts,omitempty"`
+	ID         *primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Tag        string              `json:"tag,omitempty" bson:"tag,omitempty"`
+	Name       string              `json:"name,omitempty" bson:"name,omitempty"`
+	Country    string              `json:"country,omitempty" bson:"country,omitempty"`
+	Team       *Team               `json:"team,omitempty" bson:"team,omitempty"`
+	Accounts   []*Account          `json:"accounts,omitempty" bson:"accounts,omitempty"`
+	Substitute bool                `json:"substitute,omitempty" bson:"substitute,omitempty"`
+	Coach      bool                `json:"coach,omitempty" bson:"coach,omitempty"`
 }
 
 // Account .
